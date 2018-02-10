060e8ee805bd22aacf6f2aad167b7b04be33f86c
zAndrewz <33632794+zAndrewz@users.noreply.github.com>
2018-02-10 14:46:19 +0300

    Add files via upload

---
+++ Threads/src/threads/ProducerConsumerExample.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package threads;
+
+/**
+ *
+ * @author lean
+ */
+public class ProducerConsumerExample {
+    public static void main(String[] args) {
+        Drop drop = new Drop();
+        (new Thread(new Producer(drop))).start();
+        (new Thread(new Consumer(drop))).start();
+    }
+}
